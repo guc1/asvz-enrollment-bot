215bfaa6e8625df04ce75572ca0a5be1e0ce04ab
guc1 <164617227+guc1@users.noreply.github.com>
2024-11-12 17:11:41 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,7 @@
-from app import create_app, db  # Import db from app
-import os
+# run.py
+from app import create_app
 
 app = create_app()
-
-# Create the database tables if they don't exist
-with app.app_context():
-    if not os.path.exists('site.db'):
-        db.create_all()
-        print('Database tables created.')
 
 if __name__ == '__main__':
     app.run()
